58bb1317af6519431d758e65d3a08a3a41c324a8
Osnaider Carbonell <42350856+osnacarbonelles@users.noreply.github.com>
2021-07-24 14:16:33 -0500

    agregando bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h3 className="text-center p-2">Lista de tareas</h3>
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-5">Hola Mundo!</div>
+        </div>
+      </div>
+    </>
   );
 }
 
